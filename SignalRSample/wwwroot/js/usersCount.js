65180e88f43fb8ec4a6cea2ccfee7ce2654c2cee
Vitalik <81742272+vitaliy-nerdysoft@users.noreply.github.com>
2023-02-18 23:30:38 +0200

    Second initial commit

---
+++ SignalRSample/wwwroot/js/usersCount.js
@@ -0,0 +1,34 @@
+﻿// create connection
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+//connect to methods that hub invokes aka receive notifications from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+})
+
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = value.toString();
+})
+// invoke hub metods aka send notification to hub
+
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+
+// start connection
+
+function fulfilled() {
+    // do some on start
+    console.log("connection to User hub successful");
+    newWindowLoadedOnClient();
+}
+
+function rejected() {
+    // rejected logs
+    console.log("connection to User hub rejected");
+}
+
+connectionUserCount.start().then(fulfilled, rejected);
